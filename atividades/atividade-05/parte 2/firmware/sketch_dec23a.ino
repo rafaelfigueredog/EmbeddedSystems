46083bcf1b9ebb038c0b44e957be3281bc415277
Rafael Guimarães <rafael.guimaraes@academico.ifpb.edu.br>
2021-12-23 02:24:16 -0300

    Add files of activity 5
    Atividade 5, Parte 2:
    - esquemático
    - firmware
    - sumulide file

---
+++ atividades/atividade-05/parte 2/firmware/sketch_dec23a.ino
@@ -0,0 +1,43 @@
+
+int pinLed = 7;
+int interruptPin = 2; 
+
+int config; 
+int timeOn;
+int timeOff; 
+
+void handleToInterrupt() {
+  cli();   // desable interrupts 
+  config = !config; 
+  timeOn = config == 1? 2000 : 5000; 
+  timeOff = config == 1? 1000 : 25000;
+  sei();  // enable interrupts 
+}
+
+
+void setup() {
+
+  // desable interrupts 
+  cli(); 
+  
+  pinMode(pinLed, OUTPUT);
+
+  // interrupt config 
+  pinMode(interruptPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(interruptPin), handleToInterrupt, FALLING);   
+  
+  // inicial config
+  config = 1;
+  timeOn = config == 1? 2000 : 5000; 
+  timeOff = config == 1? 1000 : 25000; 
+  
+  // enable interrupts
+  sei();                 
+}                                                              
+
+void loop(){
+  digitalWrite(pinLed, 1); 
+  delay(timeOn); 
+  digitalWrite(pinLed, 0); 
+  delay(timeOff); 
+}
